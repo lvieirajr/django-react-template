b69cbb2bc8bc3511dde80fa3156a4d79a795ea0d
Luis Antonio Vieira Junior <lvieira@lvieira.com>
2024-03-14 18:04:19 -0700

    auth0 audience

--- frontend/src/components/providers/auth0.tsx
+++ frontend/src/components/providers/auth0.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren, JSX } from "react";
 import { Auth0Provider as BaseAuth0Provider, AppState } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
-import { AUTH0_CLIENT_ID, AUTH0_DOMAIN } from "@/environment";
+import { AUTH0_AUDIENCE, AUTH0_CLIENT_ID, AUTH0_DOMAIN } from "@/environment";
 
 export function Auth0Provider({ children }: PropsWithChildren): JSX.Element {
   const navigate = useNavigate();
@@ -11,7 +11,10 @@ export function Auth0Provider({ children }: PropsWithChildren): JSX.Element {
     <BaseAuth0Provider
       domain={AUTH0_DOMAIN}
       clientId={AUTH0_CLIENT_ID}
-      authorizationParams={{ redirect_uri: window.location.origin }}
+      authorizationParams={{
+        audience: AUTH0_AUDIENCE,
+        redirect_uri: window.location.origin,
+      }}
       useRefreshTokens={true}
       cacheLocation="localstorage"
       onRedirectCallback={(appState?: AppState) =>
--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -10,5 +10,6 @@ export const DEBUG = import.meta.env.VITE_DEBUG;
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
 // Auth0
+export const AUTH0_AUDIENCE = import.meta.env.VITE_AUTH0_AUDIENCE;
 export const AUTH0_DOMAIN = import.meta.env.VITE_AUTH0_DOMAIN;
 export const AUTH0_CLIENT_ID = import.meta.env.VITE_AUTH0_CLIENT_ID;
